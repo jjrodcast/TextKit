87176ba5b7f40ca1313118e0f7f5b77fae5abc0e
jjrodcast <jorge.rodriguez@autodesk.com>
2026-07-20 15:51:33 -0500

    - Adding button to change between light and dark mode

--- shared/src/iosMain/kotlin/com/jjrodcast/textkit/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/jjrodcast/textkit/MainViewController.kt
@@ -1,8 +1,8 @@
 package com.jjrodcast.textkit
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.jjrodcast.textkit.sample.TextKitSampleNonMobile
+import com.jjrodcast.textkit.sample.TextKitSample
 
 fun MainViewController() = ComposeUIViewController {
-    TextKitSampleNonMobile()
+    TextKitSample()
 }
